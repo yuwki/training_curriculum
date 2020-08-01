60e712a81d0b1f7af0ebeba988cbe8387b0980fa
yuwki <yuuki.ishii1208@gmail.com>
2020-08-01 15:16:36 +0900

    Issue6

--- app/controllers/calendars_controller.rb
+++ app/controllers/calendars_controller.rb
@@ -38,7 +38,7 @@ class CalendarsController < ApplicationController
       plan = @plans.map do |plan|
         plans.push(plan.plan) if plan.date == @todays_date + x
       end
-      days = { month: (@todays_date + x).month, date: (@todays_date+x).day, plans: plans}
+      days = { month: (@todays_date + x).month, date: (@todays_date+x).day, plans: plans, wdays: wdays[(@todays_date+x).wday]}
       @week_days.push(days)
     end
 
